d27dae8e21c6ec494a76889d5f8330db01634690
Sasha Savchuk <sasha@bots.house>
2020-11-03 16:07:25 +0200

    feat(service/chat): handle bot is not admin

--- pkg/tg/errors.go
+++ pkg/tg/errors.go
@@ -11,3 +11,8 @@ func IsMemberListIsInaccessible(err error) bool {
 	err2, ok := err.(tgbotapi.Error)
 	return ok && err2.Message == "Bad Request: member list is inaccessible"
 }
+
+func IsBotIsNotMemberOfChat(err error) bool {
+	err2, ok := err.(tgbotapi.Error)
+	return ok && err2.Message == "Forbidden: bot is not a member of the channel chat"
+}
